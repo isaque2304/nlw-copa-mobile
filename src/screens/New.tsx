fb6f84be124e10a002c8950593829a099f013d84
isaque2304 <isaque2304@gmail.com>
2022-11-06 15:49:21 -0300

    inclusão de novos layout e novas estruturas e componentes

---
+++ src/screens/New.tsx
@@ -0,0 +1,33 @@
+import { Heading, Text, VStack } from "native-base";
+import { Header } from "../components/Header";
+import Logo from '../assets/logo.svg';
+import { Input } from "../components/Input";
+import { ButtonIcon } from "../components/ButtonIcon";
+import { Button } from "../components/Button";
+
+export function New(){
+    return (
+        <VStack flex={1} bgColor="gray.900">
+            <Header title="Criar Novo Bolão"/>
+
+            <VStack mt={8} mx={5} alignItems="center">
+                <Logo />
+
+                <Heading fontFamily="heading" color="white" fontSize="xl" my={8} textAlign="center">
+                    Crie seu próprio bolão da copa e compartilhe entre amigos!
+                </Heading>
+
+                <Input 
+                    mb={2}
+                    placeholder="Qual nome do seu bolão?"
+                />
+
+                <Button 
+
+                title="CRIAR MEU BOLÃO"/>
+            </VStack>
+
+            <Text color="gray.200" textAlign="center" mt={4} px={10}>Após criar seu bolão, você receberá um {'\n'} código único que poderá usar para convidar {'\n'} outras pessoas.</Text>
+        </VStack>
+    );
+}
